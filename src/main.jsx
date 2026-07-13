9c845fa0ef84b1c732beb36957a2560a8617c0ee
Nyaru01 <virgil.czarnecki@gmail.com>
2026-07-13 09:17:48 +0200

    Fix duplicate notification banners (#8)
    * fix: remove duplicate notification toaster (src/main.jsx)
    
    * fix: remove duplicate notification toaster (package.json)
    
    * fix: remove duplicate notification toaster (package-lock.json)
    
    * fix: remove duplicate notification toaster (vite.config.js)

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import { Toaster } from 'react-hot-toast';
 
 // @ts-ignore - Defined by Vite during build
 /* global __APP_VERSION__ */
@@ -10,7 +9,6 @@ const version = typeof __APP_VERSION__ !== 'undefined' ? __APP_VERSION__ : 'dev'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Toaster position="top-center" />
     <App version={version} />
   </React.StrictMode>,
 );
