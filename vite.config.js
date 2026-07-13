3c40e92a255a0219699c03a52a777460e702555e
Nyaru01 <virgil.czarnecki@gmail.com>
2026-07-13 09:17:45 +0200

    fix: remove duplicate notification toaster (vite.config.js)

--- vite.config.js
+++ vite.config.js
@@ -34,7 +34,7 @@ export default defineConfig({
         name: 'Skyjo Score V2',
         short_name: 'SkyjoScore',
         description: 'Calculateur de score & Hub Social pour Skyjo',
-        version: '3.3.2',
+        version: '3.3.3',
         theme_color: '#6366f1',
         background_color: '#0f172a',
         display: 'standalone',
